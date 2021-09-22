8fdee23e75e72b122975fe668796c1aa2036fd35
kyaido <kamehameha1985@gmail.com>
2021-09-22 23:07:49 +0900

    fix to new rule format
    https://eslint.org/blog/2016/07/eslint-new-rule-format

--- rules/literal-blacklist.js
+++ rules/literal-blacklist.js
@@ -1,49 +1,52 @@
 'use strict';
 
-module.exports = function(context) {
-
-  let options = [];
-  if (Array.isArray(context.options[0])) {
-    options = context.options[0];
-  }
-
-  return {
-    'Literal': node => {
-      const value = String(node.value);
-
-      options.forEach(option => {
-        const isStringOption = typeof option === 'string';
-        const term = isStringOption ? option : option.term;
-
-        if (value.indexOf(term) !== -1) {
-          const message = isStringOption ? `You should not use "${term}".` : option.message;
-          context.report({node, message});
-        }
-      });
-    }
-  };
-};
-
-module.exports.schema = [{
-  type: 'array',
-  items: {
-    oneOf: [
-      {
-        type: "string",
-      },
-      {
-        type: "object",
-        properties: {
-          term: {
-            type: "string",
+module.exports = {
+  meta: {
+    schema: [{
+      type: 'array',
+      items: {
+        oneOf: [
+          {
+            type: 'string',
           },
-          message: {
-            type: "string",
+          {
+            type: 'object',
+            properties: {
+              term: {
+                type: 'string',
+              },
+              message: {
+                type: 'string',
+              }
+            },
+            additionalProperties: false
           }
-        },
-        additionalProperties: false
-      }
-    ]
+        ]
+      },
+      uniqueItems: true
+    }]
   },
-  uniqueItems: true
-}];
+
+  create: (context) => {
+    let options = [];
+    if (Array.isArray(context.options[0])) {
+      options = context.options[0];
+    }
+
+    return {
+      'Literal': node => {
+        const value = String(node.value);
+
+        options.forEach(option => {
+          const isStringOption = typeof option === 'string';
+          const term = isStringOption ? option : option.term;
+
+          if (value.indexOf(term) !== -1) {
+            const message = isStringOption ? `You should not use '${term}'.` : option.message;
+            context.report({node, message});
+          }
+        });
+      }
+    };
+  }
+};
