a0aa86303fed59968e6c59393b59c91723c1b0df
kyaido <kamehameha1985@gmail.com>
2021-09-22 23:34:52 +0900

    modify test

--- tests/literal-blacklist.js
+++ tests/literal-blacklist.js
@@ -13,27 +13,32 @@ tester.run('literal-blacklist', require('../rules/literal-blacklist'), {
     {
       code: 'var http = "http://example.com";',
       options: [['http:']],
-      errors: ['You should not use "http:".'],
+      errors: [`You should not use 'http:'.`],
     },
     {
       code: 'document.body.innerHTML("<script src=\'http://example.com\'></script>");',
       options: [['http:']],
-      errors: ['You should not use "http:".'],
+      errors: [`You should not use 'http:'.`],
     },
     {
       code: 'var obj = { url: "http://example.com" };',
       options: [['http:']],
-      errors: ['You should not use "http:".'],
+      errors: [`You should not use 'http:'.`],
     },
     {
       code: 'var arr = [ "https://example.com", "http://example.com" ];',
       options: [['http:']],
-      errors: ['You should not use "http:".'],
+      errors: [`You should not use 'http:'.`],
     },
     {
       code: '(function() { return "http://example.com" })();',
       options: [['http:']],
-      errors: ['You should not use "http:".'],
+      errors: [`You should not use 'http:'.`],
+    },
+    {
+      code: 'var http = "http://example.com";',
+      options: [[{ term: 'http:', message: `You should use 'https:' instead of 'http:.'`}]],
+      errors: [`You should use 'https:' instead of 'http:.'`],
     },
   ],
 });
